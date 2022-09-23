db147d29524a245e00cfeb4e642128fa18868ce7
Matheus Amaral <m.amaral8998@gmail.com>
2022-09-23 13:31:48 -0300

    Jogada especial promoção

--- Xadrez-Console/XadrezEntities/PartidaDeXadrez.cs
+++ Xadrez-Console/XadrezEntities/PartidaDeXadrez.cs
@@ -142,7 +142,22 @@ namespace XadrezEntities
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
-            if(EstaEmXeque(Adversaria(JogadorAtual)))
+            Peca p = Tab.Peca(destino);
+
+            //#jogadaespecial promoção
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
+            if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
             }
@@ -161,7 +176,7 @@ namespace XadrezEntities
                 MudaJogador();
             }
 
-            Peca p = Tab.Peca(destino);
+            
 
             // #jogadaespecial en passant
             if(p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
